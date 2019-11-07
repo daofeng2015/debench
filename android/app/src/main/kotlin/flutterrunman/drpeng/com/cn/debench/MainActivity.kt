99717da52528362567f56a41fc47a828712f531c
daofeng2015 <jcb20150301@gmail.com>
2019-11-07 16:09:05 +0800

    初始化项目

---
+++ android/app/src/main/kotlin/flutterrunman/drpeng/com/cn/debench/MainActivity.kt
@@ -0,0 +1,13 @@
+package flutterrunman.drpeng.com.cn.debench
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
